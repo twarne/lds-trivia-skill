484b2758980e578d7afcbd5550a75cec4d8910c5
Tom Warne <twarne@gmail.com>
2017-06-07 22:21:51 -0700

    Update game instruction phrases.

--- src/lib/resources.js
+++ src/lib/resources.js
@@ -9,15 +9,16 @@ module.exports = {
 			QUESTIONS: questions['questions'],
 			GAME_NAME: 'LDS Trivia', // Be sure to change this for your skill.
 			HELP_MESSAGE:
-				'I will ask you %s multiple choice questions. Respond with the number of the answer. ' +
-					'For example, say one, two, three, or four. To start a new game at any time, say, start game. ',
+				'I will ask you %s questions. Respond with the number of the answer. ' +
+					'For example, say, is it Gordon B Hinckley? Or, the answer is 1985. ' +
+					'To start a new game at any time, say, start game. ',
 			REPEAT_QUESTION_MESSAGE: 'To repeat the last question, say, repeat. ',
 			ASK_MESSAGE_START: 'Would you like to start playing?',
-			HELP_REPROMPT: 'To give an answer to a question, respond with the number of the answer. ',
+			HELP_REPROMPT: "To give an answer to a question, respond with a phrase like, 'Is it Joseph Smith?'. ",
 			STOP_MESSAGE: 'Would you like to keep playing?',
 			CANCEL_MESSAGE: "Ok, let's play again soon.",
 			NO_MESSAGE: "Ok, we'll play another time. Goodbye!",
-			TRIVIA_UNHANDLED: 'Try saying a number between 1 and %s',
+			TRIVIA_UNHANDLED: 'Try saying an answer phrase.',
 			HELP_UNHANDLED: 'Say yes to continue, or no to end the game.',
 			START_UNHANDLED: 'Say start to start a new game.',
 			NEW_GAME_MESSAGE: 'Welcome to %s. ',
@@ -27,7 +28,7 @@ module.exports = {
 			CORRECT_ANSWER_MESSAGE: 'The correct answer is: %s. ',
 			ANSWER_IS_MESSAGE: 'That answer is ',
 			TELL_QUESTION_MESSAGE: 'Question %s. %s ',
-			GAME_OVER_MESSAGE: 'You got %s out of %s questions correct. Thank you for playing!',
+			GAME_OVER_MESSAGE: 'You got %s out of %s questions correct. Thank you for playing LDS Trivia!',
 			SCORE_IS_MESSAGE: 'Your score is %s. '
 		}
 	},
